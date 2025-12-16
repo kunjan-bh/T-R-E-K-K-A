0f7851dcdb60bd3fb1ad035e08a627d0adff983f
sonu <skjais04@gmail.com>
2025-12-16 13:01:12 +0545

    added accounts admin panel

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+
+class UserAdmin(BaseUserAdmin):
+    """
+    Custom admin for User model
+    """
+    list_display = ('email', 'first_name', 'last_name', 'is_from_nepal', 'is_staff', 'date_joined')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'is_from_nepal')
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'is_from_nepal')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_from_nepal'),
+        }),
+    )
+    
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('-date_joined',)
+    filter_horizontal = ('groups', 'user_permissions',)
+
+
+admin.site.register(User, UserAdmin)
