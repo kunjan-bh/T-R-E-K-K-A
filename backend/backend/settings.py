aa057192897366d935a0c84a9a0f62db8616f3ff
Kunjan Bhatt <bhattakunjan10@gmail.com>
2025-12-31 16:06:46 +0545

    Hide smtp

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -167,8 +167,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'bhattakunjan10@gmail.com'
-EMAIL_HOST_PASSWORD = 'regz jxzv egyh ykiy'  # Gmail app password if 2FA enabled
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 
 # Modern allauth configuration (replaces deprecated settings)
